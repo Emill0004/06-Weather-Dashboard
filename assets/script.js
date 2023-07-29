28b67c249e63e3ad527346ca7a516045b6320e49
Eric Miller <e.s.miller13125@gmail.com>
2023-07-29 16:28:45 -0600

    added code to make api calls based on user input

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const apiKey = '8f2126d969ed7632e1abebe4121c44a9'
-const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${apiKey}`
-const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`
+const searchBar = document.getElementById('searchBar');
+const searchBtn = document.getElementById('searchBtn');
 
 function getweather(url) {
     fetch(url)
@@ -8,9 +8,9 @@ function getweather(url) {
             return response.json();
         })
         .then(function (data) {
-            stockList = data.results;
-            localStorage.setItem("Weather_Info", JSON.stringify(stockList));
-            console.log(data);
+            weatherData = data;
+            localStorage.setItem("Weather_Info", JSON.stringify(weatherData));
+            console.log(weatherData);
         })
 };
 
@@ -20,18 +20,21 @@ function getCity(url) {
             return response.json();
         })
         .then(function (data) {
-            cityCoord = data.results;
-            console.log(data);
-            console.log("lat: " + data[0].lat + " lon: " + data[0].lon);
+            cityData = data;
+            localStorage.setItem("City_Info", JSON.stringify(cityData));
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityData[0].lat}&lon=${cityData[0].lon}&appid=${apiKey}`;
+            getweather(weatherUrl);
         })
 }
 
-// getweather(weatherUrl);
-getCity(cityUrl);
+searchBtn.addEventListener("click", function(event) {
+    event.preventDefault;
+    const cityUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchBar.value}&limit=1&appid=${apiKey}`;
+    getCity(cityUrl);
+})
 
 /*
 TODOS:
-Find the API for cities to lat/long
-Connect the two APIs to enable user searches
 populate the data in index.html
 */
